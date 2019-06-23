03a78596a2bb80a6e48bb9543862332d8630854d
lozeCo <jatateon@gmail.com>
2019-06-23 14:04:58 -0500

    El componente tablero, con la funcionalidad de agregar

--- src/App.js
+++ src/App.js
@@ -7,19 +7,29 @@ class App extends React.PureComponent {
     state = {
         boards:[
             {
-                name: 'girls',
+                title: 'girls',
                 items: ['Monica','Julieta','María','Antonieta'],
                 index: 0,
+                label: 'Siguente',
+                input: {
+                    add:'Maria',
+                    remove:''
+                },
             },
             {
-                name: 'food',
+                title: 'food',
                 items: ['mole','chiles rellenos','pizza','spaguetti','memelas'],
                 index: 0,
+                label: 'Siguente',
+                input:{
+                    add:'Chilaquiles',
+                    remove:''
+                },
             }
         ]
     };
 
-    onHandleButton = (object, key) => {
+    onHandleButton = (key) => {
         const nextState = produce(this.state, (draft) => {
             if(draft.boards[key].items.length > draft.boards[key].index + 1) {
                 draft.boards[key].index = draft.boards[key].index + 1;
@@ -27,6 +37,35 @@ class App extends React.PureComponent {
             else {
                 draft.boards[key].index = 0;
             } 
+        });
+        this.setState(nextState);
+    };
+
+    onInputChange = (event, index, type) => {
+        const value = event.target.value
+        const nextState = produce( this.state, (draft) => {
+            draft.boards[index].input[type] = value;
+        });
+        this.setState(nextState);
+    };
+
+    onAddButtonClick = (index) => {
+        const nextState = produce(this.state, (draft)=>{
+            if(draft.boards[index].input.add.length > 0) {
+                draft.boards[index].items = draft.boards[index].items.concat([draft.boards[index].input.add]);
+            } 
+            draft.boards[index].input.add = '';
+        });
+        this.setState(nextState);
+    };
+
+    onRemoveButtonClick = (index) => {
+        const nextState = produce(this.state, (draft) => {
+            if(draft.boards[index].input.remove.length > 0 ){
+                let i = draft.boards[index].input.remove.value;
+                draft.boards[index].items.splice(i,1);
+                draft.boards[index].input.remove = '';
+            }
         });
         this.setState(nextState);
     };
@@ -40,18 +79,19 @@ class App extends React.PureComponent {
                         {boards.map((board,i) => {                            
                             return (
                             <Board 
-                                key={i} 
-                                items={board.items} 
-                                index={board.index} 
-                                label={'Siguiente'} 
-                                onButtonClick={()=> this.onHandleButton(board,i)} 
+                                object={board}
+                                index={i}
+                                onButtonClick={()=> this.onHandleButton(i)} 
+                                onChangeInput={this.onInputChange}
+                                onAddClick={this.onAddButtonClick}
+                                onRemoveClick={this.onRemoveButtonClick}
                             />)
                         })}
                     </div>
                 <p className={styles.result}>Los elementos seleccionados son: 
                         {boards.map((board) => {
                             return (
-                               <p>{board.items[board.index]}</p>
+                                <p>{board.items[board.index]}</p>
                             )
                         })}
                 </p>
